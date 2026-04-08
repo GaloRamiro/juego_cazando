501799b8a9763a56da2d9f176ce3f8afd4e86d5d
GaloRamiro <g_cisneros_23@hotmail.es>
2026-04-08 17:01:35 -0500

    Se implementa iniciarJuego, se dibuja gato y comida, y se agregan variables y constantes de posición y tamaño

--- cazando.js
+++ cazando.js
@@ -1,15 +1,42 @@
 /* obtener el canbas y su contexto para dibujar*/
 let canvas=document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
+/*Definir Variables*/
+let gatoX=0;
+let gatoY=0;
+let comidaX=100;
+let comidaY=100;
+
+/*Definir Constantes*/
+const ALTO_GATO=50;
+const ANCHO_GATO=50;
+
+const ANCHO_COMIDA=30;
+const ALTO_COMIDA=30;
+
 
 /* obtener el canbas y su contexto para dibujar*/
-function graficarGato(){
-    let ancho = 50;
-    let alto = 50;
 
-    let x = (canvas.width - ancho) / 2;
-    let y = (canvas.height - alto) / 2;
+function IniciarJuego(){
+ gatoX = (canvas.width / 2) - (ANCHO_GATO / 2); 
+ gatoY = (canvas.height / 2) - (ALTO_GATO/ 2);
 
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(x, y, ancho, alto);
+ graficarGato();
+ graficarComida();
 }
+
+function graficar(x,y,ancho,alto,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,ancho,alto);
+}
+
+
+function graficarGato(){
+     graficar(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "#000000");
+}
+
+function graficarComida(){
+     graficar(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "#df1111");
+}
+
+
